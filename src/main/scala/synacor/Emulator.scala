34a8bd58102840ed794df9cddb9f53550cf3483e
Stewart Stewart <stewinsalot@gmail.com>
2025-12-18 06:05:39 -0500

    simplify synacor emulator

--- src/main/scala/synacor/Emulator.scala
+++ src/main/scala/synacor/Emulator.scala
@@ -1,38 +1,27 @@
 package synacor
 
-def feedInput(start: Tick, input: String): Tick =
-  assert(input.last == '\n')
-  // assert(start.isInstanceOf[Tick.Input])
-  input.foldLeft(start):
-    case (tick, c) =>
-      Iterator.iterate(tick):
-        case Tick.Continue(state) => state.tick
-        case Tick.Input(f) => f(c).tick // dummy ticks
-      .collectFirst:
-        case Tick.Input(f) => f(c).tick
-      .getOrElse:
-        throw new Exception(s"input too long: $input")
-
-def progressCollectingOutput(start: Tick): (String, Tick) =
-  val sb = collection.mutable.StringBuilder()
-  var tick = start
-
-  while !tick.needsInput do
-    tick match
-      case Tick.Continue(state) => tick = state.tick
-      case Tick.Output(c, state) =>
-        sb.addOne(c)
-        tick = state.tick
-      case Tick.Input(f) => ()
-      case Tick.Halt(code, last) => ()
-
-  sb.result() -> tick
+import collection.immutable.Queue
 
 case class Emulator(state: Tick, history: List[Tick]):
   def feed(input: String): Emulator =
-    val next = feedInput(state, input)
+    assert(input.indexOf('\n') == input.length - 1)
+
+    def loop(tick: Tick, chars: List[Char]): Tick = tick match
+      case Tick.Continue(state) => loop(state.tick, chars)
+      case Tick.Input(f) => chars match
+        case c::cs => loop(f(c).tick, cs)
+        case Nil => throw new Exception("Not enough input.") // dead code
+      case tick => if chars.isEmpty then tick else
+        throw new Exception(s"Input too long.") // dead code
+
+    val next = loop(state, input.toList)
     Emulator(next, state::history)
 
   def nextOut: (String, Emulator) =
-    val (out, next) = progressCollectingOutput(state)
+    def loop(tick: Tick, chars: Queue[Char]): (String, Tick) = tick match
+      case Tick.Continue(state) => loop(state.tick, chars)
+      case Tick.Output(c, state) => loop(state.tick, chars.enqueue(c))
+      case _ => chars.mkString -> tick
+
+    val (out, next) = loop(state, Queue.empty)
     out -> Emulator(next, state::history)
