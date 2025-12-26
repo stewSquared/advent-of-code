83ac34adb845bd6e038b22bf051f031442e86442
Stewart Stewart <stewinsalot@gmail.com>
2025-12-26 01:12:05 -0500

    Move Opcode Enum to Inst.scala

--- src/main/scala/synacor/Inst.scala
+++ src/main/scala/synacor/Inst.scala
@@ -2,6 +2,24 @@ package synacor
 
 import numbers.*
 
+enum Opcode:
+  case HALT, SET
+  case PUSH, POP
+  case EQ, GT
+  case JMP, JT, JF
+  case ADD, MULT, MOD
+  case AND, OR, NOT
+  case RMEM, WMEM
+  case CALL, RET
+  case OUT, IN
+  case NOOP
+
+  def numParams: Int = this match
+    case EQ | GT | ADD | MULT | MOD | AND | OR => 3
+    case SET | JT | JF | NOT | RMEM | WMEM => 2
+    case PUSH | POP | JMP | CALL | OUT | IN => 1
+    case _ => 0
+
 enum Arg[T <: U15]:
   case Const(value: T)
   case Ref(reg: Reg)
--- src/main/scala/synacor/VMState.scala
+++ src/main/scala/synacor/VMState.scala
@@ -2,24 +2,6 @@ package synacor
 
 import synacor.numbers.*
 
-enum Opcode:
-  case HALT, SET
-  case PUSH, POP
-  case EQ, GT
-  case JMP, JT, JF
-  case ADD, MULT, MOD
-  case AND, OR, NOT
-  case RMEM, WMEM
-  case CALL, RET
-  case OUT, IN
-  case NOOP
-
-  def numParams: Int = this match
-    case EQ | GT | ADD | MULT | MOD | AND | OR => 3
-    case SET | JT | JF | NOT | RMEM | WMEM => 2
-    case PUSH | POP | JMP | CALL | OUT | IN => 1
-    case _ => 0
-
 case class Registers(underlying: IArray[U15]):
   require(underlying.sizeIs == 8)
   def apply(r: Reg): Word = underlying(r.toIndex)
