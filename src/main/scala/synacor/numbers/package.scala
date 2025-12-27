5f5df9d40ef6f511af2f3c99512f1f9ce31e52ed
Stewart Stewart <stewinsalot@gmail.com>
2025-12-27 04:46:55 -0500

    push number extension methods into companion objects

--- src/main/scala/synacor/numbers/package.scala
+++ src/main/scala/synacor/numbers/package.scala
@@ -36,33 +36,33 @@ object U15:
     require(Word.fitsU15(w), s"${w.hex} is not a valid U15")
     w
 
-extension (v: U15)
-  inline def hex: String = f"0x${v}%04X"
-  inline def asAdr: Adr = v
-  inline def asLit: Lit = v
+  extension (v: U15)
+    inline def hex: String = f"0x${v}%04X"
+    inline def asAdr: Adr = v
+    inline def asLit: Lit = v
 
 object Adr:
   def parse(w: Word): Adr = U15.parse(w)
 
-extension (a: Adr)
-  inline def inc1: Adr = a + 1
-  inline def inc2: Adr = a + 2
-  inline def inc3: Adr = a + 3
-  inline def inc4: Adr = a + 4
-  inline def toIndex: Int = a & 0x7FFF
+  extension (a: Adr)
+    inline def inc1: Adr = a + 1
+    inline def inc2: Adr = a + 2
+    inline def inc3: Adr = a + 3
+    inline def inc4: Adr = a + 4
+    inline def toIndex: Int = a & 0x7FFF
 
 object Lit:
   def parse(w: Word): Lit = U15.parse(w)
 
-extension (a: Lit)
-  infix def +(b: Lit): Lit = (a + b) & U15.MaxValue
-  infix def *(b: Lit): Lit = (a * b) & U15.MaxValue
-  infix def %(b: Lit): Lit = (a % b) & U15.MaxValue
-  infix def &(b: Lit): Lit = (a & b) & U15.MaxValue
-  infix def |(b: Lit): Lit = (a | b) & U15.MaxValue
-  infix def >(b: Lit): Boolean = a > b
-  def unary_~ = ((~a) & U15.MaxValue): Lit
-  inline def toChar: Char = a.toChar
+  extension (a: Lit)
+    infix def +(b: Lit): Lit = (a + b) & U15.MaxValue
+    infix def *(b: Lit): Lit = (a * b) & U15.MaxValue
+    infix def %(b: Lit): Lit = (a % b) & U15.MaxValue
+    infix def &(b: Lit): Lit = (a & b) & U15.MaxValue
+    infix def |(b: Lit): Lit = (a | b) & U15.MaxValue
+    infix def >(b: Lit): Boolean = a > b
+    def unary_~ = ((~a) & U15.MaxValue): Lit
+    inline def toChar: Char = a.toChar
 
 enum Reg:
   case R1, R2, R3, R4, R5, R6, R7, R8
@@ -74,6 +74,6 @@ object Reg:
 
   def fromIndex(i: Int): Reg = Reg.fromOrdinal(i)
 
-extension (r: Reg)
-  inline def toIndex: Int = r.ordinal
-  inline def name = r.toString
+  extension (r: Reg)
+    inline def toIndex: Int = r.ordinal
+    inline def name = r.toString
